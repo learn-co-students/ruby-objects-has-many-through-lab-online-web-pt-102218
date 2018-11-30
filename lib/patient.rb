22a0728b54b7f8b85fa2399643da8f2a0f88ebc6
Linda Chang <lindachang186@gmail.com>
2018-11-30 21:29:17 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,5 @@
 class Patient
+
   attr_accessor :name
 
   @@all = []
@@ -11,5 +12,24 @@ class Patient
   def self.all
     @@all
   end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+      appointment.doctor
+    end
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+  end
+
+  def doctors
+    Appointment.all.collect do |appointment|
+      appointment.patient == self
+      appointment.doctor
+    end
+  end
+
 
 end
