f3ea9815a5538fb0123f4de4f1dc948c8f9ce7bb
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-04 16:26:45 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+class Patient 
+  
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+    @appointment = []
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(doctor, date) 
+    new = Appointment.new(self, doctor, date)
+    @appointment << new 
+    new 
+  end 
+  
+  def appointments 
+    @appointment
+  end 
+  
+  def doctors 
+    @appointment.map do |i| 
+      i.doctor 
+    end 
+  end 
+  
+end 
