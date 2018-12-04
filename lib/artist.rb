5909e9977b9098d459e7e519c5d8ad7fe264798f
Dakota Clark <bannaray95@gmail.com>
2018-12-04 20:50:38 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre) 
+    song = Song.new(name, self, genre)
+    @songs << song 
+    song
+  end 
+  
+  def songs 
+    @songs 
+  end 
+  
+  def genres 
+    songs.map do |i| 
+      i.genre
+    end 
+  end
+  
+end 
